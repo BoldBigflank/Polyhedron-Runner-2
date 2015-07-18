c2b4696768fb2c451c41437d3196647f29e9a0ae
Alex Swan <alex@bold-it.com>
2015-07-18 13:37:34 -0700

    Initial

---
+++ Assets/Scripts/OrbitTargetScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OrbitTargetScript : MonoBehaviour {
+	public float distance = 10.0F;
+	public Transform target;
+	
+	// Use this for initialization
+	void Start () {
+	
+		if(!target) target = new GameObject().transform;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		Quaternion rotation = transform.rotation;
+		Vector3 position = rotation * (distance * Vector3.back) + target.position;
+		
+		transform.position = position;
+	}
+}
---
+++ Assets/Scripts/RecordOrientationScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RecordOrientationScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.touches.Length > 0) {
+			Touch t = Input.GetTouch(0);
+			if(t.phase == TouchPhase.Began){
+				
+			}
+		
+		}
+	}
+}
